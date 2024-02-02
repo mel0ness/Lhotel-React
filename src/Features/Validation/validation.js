2f2b5bc99e970c6d443f701fae60fa9b9d33cf7d
prof <spasmodic51@gmail.com>
2024-02-02 22:29:18 +0100

    Actu des fermetures

--- src/Features/Validation/validation.js
+++ src/Features/Validation/validation.js
@@ -8,14 +8,9 @@ if(Date.parse(e) < Date.parse(tomorrow) || Date.parse(e) >= Date.parse(h)) {
     f(true)
     g(false)
 }
-else if (Date.parse("2024-02-05") < Date.parse(e)) {
+else if (Date.parse(e) < Date.parse("2024-02-05") ) {
 
-f(false)    
-g(false)
-}
-else if (Date.parse("2024-02-05") >= Date.parse(e)) {
-
-f(false)    
+f(true)    
 g(true)
 }
 // else if (Date.parse("2023-10-29") < Date.parse(h) && Date.parse(h) <= Date.parse("2023-11-06")) {
